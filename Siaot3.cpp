acf5dbaffd9840d7a5dabce615d6889c9abd2b2a
LaimOr127 <95967934+LaimOr127@users.noreply.github.com>
2023-05-18 23:31:48 +0300

    1

---
+++ Siaot3.cpp
@@ -0,0 +1,117 @@
+﻿#include <iostream>
+#include <cstring>
+using namespace std;
+
+const int MAX_SIZE = 100; // максимальный размер стека
+
+class Stack {
+private:
+    char data[MAX_SIZE]; // массив для хранения элементов стека
+    int top_index; // индекс верхнего элемента стека
+
+public:
+    Stack() { // конструктор по умолчанию
+        top_index = -1;
+    }
+
+    void push(char value) { // добавление элемента в стек
+        if (top_index == MAX_SIZE - 1) { // проверка на переполнение
+            cout << "Error: stack overflow" << endl;
+            return;
+        }
+        top_index++;
+        data[top_index] = value;
+    }
+
+    char pop() { // удаление элемента из стека
+        if (top_index == -1) { // проверка на пустоту
+            cout << "Error: stack is empty" << endl;
+            return '\0';
+        }
+        char value = data[top_index];
+        top_index--;
+        return value;
+    }
+
+    char top() { // получение верхнего элемента стека
+        if (top_index == -1) { // проверка на пустоту
+            cout << "Error: stack is empty" << endl;
+            return '\0';
+        }
+        return data[top_index];
+    }
+
+    bool empty() { // проверка, пуст ли стек
+        return top_index == -1;
+    }
+};
+
+int priority(char op) { // определение приоритета оператора
+    switch (op) {
+    case '+':
+    case '-':
+        return 1;
+    case '*':
+    case '/':
+        return 2;
+    case '^':
+        return 3;
+    default:
+        return 0;
+    }
+}
+
+bool is_digit(char c) { // проверка, является ли символ цифрой
+    return c >= '0' && c <= '9';
+}
+
+bool is_operator(char c) { // проверка, является ли символ оператором
+    return c == '+' || c == '-' || c == '*' || c == '/' || c == '^';
+}
+
+string infix_to_prefix(string infix) { // преобразование инфиксной записи в префиксную
+    Stack operators, result; // стек для операторов и результирующий стек
+    int n = infix.length();
+    for (int i = n - 1; i >= 0; i--) { // сканируем выражение справа налево
+        char c = infix[i];
+        if (is_digit(c)) { // если символ - число, добавляем его в результирующий стек
+            result.push(c);
+        }
+        else if (is_operator(c)) { // если символ - оператор
+            while (!operators.empty() && priority(operators.top()) >= priority(c)) {
+                // выталкиваем все операторы с большим или равным приоритетом в результирующий стек
+                result.push(operators.pop());
+            }
+            operators.push(c); // добавляем текущий оператор в стек операторов
+        }
+        else if (c == ')') { // если символ - закрывающая скобка
+            operators.push(c); // добавляем ее в стек операторов
+        }
+        else if (c == '(') { // если символ - открывающая скобка
+            while (operators.top() != ')') {
+                // выталкиваем операторы до ближайшей закрывающей скобки в результирующий стек
+                result.push(operators.pop());
+            }
+            operators.pop(); // удаляем закрывающую скобку из стека операторов
+        }
+    }
+    while (!operators.empty()) {
+        // выталкиваем все оставшиеся операторы в результирующий стек
+        result.push(operators.pop());
+    }
+    string prefix;
+    while (!result.empty()) {
+        // переворачиваем результирующий стек и собираем его элементы в строку
+        prefix += result.pop();
+    }
+    return prefix;
+}
+
+int main() {
+    string infix;
+    cout << "Enter infix expression: ";
+    getline(cin, infix);
+    string prefix = infix_to_prefix(infix);
+    cout << "Prefix expression: " << prefix << endl;
+    return 0;
+}
---
+++ Сиаод5.cpp
@@ -0,0 +1,220 @@
+﻿////Задание 1
+//#include <iostream>
+//#include <ctime>
+//#include <chrono>
+//
+//using namespace std;
+//
+//struct Element
+//{
+//    int value;
+//};
+//
+//void selectionSort(int n, Element* arr)
+//{
+//    auto begin = chrono::steady_clock::now();
+//    int max, temp, index, comparisons = 0, swaps = 0;
+//    for (int i = 0; i < n; i++)
+//    {
+//        comparisons++;
+//        max = arr[i].value;
+//        for (int j = i; j < n; j++)
+//        {
+//            comparisons++;
+//            if (arr[j].value <= max)
+//            {
+//                swaps++;
+//                max = arr[j].value;
+//                index = j;
+//            }
+//        }
+//        temp = arr[i].value;
+//        arr[i].value = arr[index].value;
+//        arr[index].value = temp;
+//    }
+//    auto end = chrono::steady_clock::now();
+//    auto elapsed_ms = chrono::duration_cast<chrono::milliseconds>(end - begin);
+//    cout << "Затраченное время: " << elapsed_ms.count() << " мс" << endl;
+//    cout << "Количество сравнений: " << comparisons << ", количество обменов: " << swaps << endl;
+//}
+//
+//int main()
+//{
+//    setlocale(LC_ALL, "Russian");
+//    srand(time(0));
+//    int n;
+//    while (true)
+//    {
+//        cout << endl;
+//        cout << "Введите количество элементов массива: ";
+//        cin >> n;
+//        Element* arr = new Element[n + 1];
+//        for (int i = 0; i < n; i++)
+//        {
+//            arr[i].value = rand();
+//        }
+//        /*for (int i = 0; i < n; i++)
+//        {
+//            cout<<arr[i].value<<endl;
+//        }*/
+//        selectionSort(n, arr);
+//        /*for (int i = 0; i < n; i++)
+//        {
+//            cout << arr[i].value << endl;
+//        }*/
+//    }
+//}
+
+
+//Задание 2
+#include <iostream>
+#include <ctime>
+#include <chrono>
+using namespace std;
+
+struct Element
+{
+    int value;
+};
+
+void sortAscending(int n, Element* arr)
+{
+    auto begin = chrono::steady_clock::now();
+    int maxVal, temp, index, compares = 0, swaps = 0;
+    for (int i = 0; i < n; i++)
+    {
+        compares++;
+        maxVal = arr[i].value;
+        index = i;
+        for (int j = i; j < n; j++)
+        {
+            compares++;
+            if (arr[j].value <= maxVal)
+            {
+                maxVal = arr[j].value;
+                index = j;
+            }
+        }
+        if (index != i)
+        {
+            temp = arr[i].value;
+            arr[i].value = arr[index].value;
+            arr[index].value = temp;
+            swaps++;
+        }
+    }
+    auto end = chrono::steady_clock::now();
+    auto elapsed_ms = chrono::duration_cast<chrono::milliseconds>(end - begin);
+    cout << "Затраченное время: " << elapsed_ms.count() << " мс" << endl;
+    cout << "Количество сравнений: " << compares << ", Количество перестановок: " << swaps << endl;
+}
+
+void sortDescending(int n, Element* arr)
+{
+    auto begin = chrono::steady_clock::now();
+    int maxVal, temp, index, compares = 0, swaps = 0;
+    for (int i = 0; i < n; i++)
+    {
+        compares++;
+        maxVal = arr[i].value;
+        index = i;
+        for (int j = i; j < n; j++)
+        {
+            compares++;
+            if (arr[j].value >= maxVal)
+            {
+                maxVal = arr[j].value;
+                index = j;
+            }
+        }
+        if (index != i)
+        {
+            temp = arr[i].value;
+            arr[i].value = arr[index].value;
+            arr[index].value = temp;
+            swaps++;
+        }
+    }
+    auto end = chrono::steady_clock::now();
+    auto elapsed_ms = chrono::duration_cast<chrono::milliseconds>(end - begin);
+    cout << "Затраченное время: " << elapsed_ms.count() << " мс" << endl;
+    cout << "Количество сравнений: " << compares << ", Количество перестановок: " << swaps << endl;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    srand(time(0));
+    int n;
+    while (true)
+    {
+        cout << "Введите количество элементов массива: ";
+        cin >> n;
+        Element* arr = new Element[n + 1];
+        for (int i = 0; i < n; i++)
+        {
+            arr[i].value = rand();
+        }
+        sortAscending(n, arr);
+        sortAscending(n, arr);
+        sortDescending(n, arr);
+        sortAscending(n, arr);
+    }
+}
+
+
+
+
+//Задача 3
+//#include <iostream>
+//#include <ctime>
+//#include <chrono>
+//using namespace std;
+//
+//struct massive
+//{
+//    int value;
+//};
+//
+//void sort(int n, massive* M)
+//{
+//    long long comparisons = 0;
+//    long long swaps = 0;
+//    auto begin = chrono::steady_clock::now();
+//    for (int i = 0; i < n - 1; i++)
+//    {
+//        comparisons++;
+//        for (int j = 0; j < n - i - 1; j++)
+//        {
+//            comparisons++;
+//            if (M[j].value > M[j + 1].value)
+//            {
+//                swaps++;
+//                swap(M[j].value, M[j + 1].value);
+//            }
+//        }
+//    }
+//    auto end = chrono::steady_clock::now();
+//    auto elapsed_ms = chrono::duration_cast<chrono::milliseconds>(end - begin);
+//    cout << "Затраченное время: " << elapsed_ms.count() << endl;
+//    cout << "Количество сравнений: " << comparisons << ", Количество обменов: " << swaps << endl;
+//}
+//
+//int main()
+//{
+//    setlocale(LC_ALL, "Russian");
+//    srand(time(0));
+//    int n;
+//    while (true)
+//    {
+//        cout << endl;
+//        cout << "Введите количество элементов массива: ";
+//        cin >> n;
+//        massive* M = new massive[n + 1];
+//        for (int i = 0; i < n; i++)
+//        {
+//            M[i].value = rand();
+//        }
+//        sort(n, M);
+//    }
+//}
